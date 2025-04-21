10d07bed7aaf92fdaac3ca9cd291da9d9b8e8c76
_GoNz4_ <chefburongonzalo@hotmail.com>
2025-04-21 19:41:55 -0300

    agregue los ejercicios 10 y 11

---
+++ JS/10-ejercicio.js
@@ -0,0 +1,15 @@
+const num1 = parseInt (prompt("ingrese un numero cualquiera"))
+
+if (num1 % 2 === 0){
+    document.writeln("<br>el numero es divisible en 2</br>")
+}else if (num1 % 3 === 0) {
+    document.writeln("el numero es divisible en 3")
+
+} else if (num1 % 5 === 0) {
+    document.writeln("el numero es divisible en 5")
+
+} else if (num1 % 7 === 0) {
+    document.writeln("el numero es divisible en 7")
+}
+    
+    
---
+++ JS/11-ejercicio.js
@@ -0,0 +1,14 @@
+const num1 = parseInt (prompt("ingrese un numero cualquiera"))
+
+if (num1 % 2 === 0){
+    document.writeln("<br>el numero es divisible en 2</br>")
+} 
+if (num1 % 3 === 0) {
+    document.writeln("<br>el numero es divisible en 3</br>")
+}
+ if (num1 % 5 === 0) {
+    document.writeln("<br>el numero es divisible en 5</br>")
+}
+ if (num1 % 7 === 0) {
+    document.writeln("<br>el numero es divisible en 7</br>")
+}
